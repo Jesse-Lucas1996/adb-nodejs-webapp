6900e814e5ebf7f23223b49a9f3e9262313f84c1
Jesse Lucas <jlucas@corpcloud.com.au>
2021-04-14 17:53:33 +0800

    just a placeholder in index

--- src/api/middleware/index.ts
+++ src/api/middleware/index.ts
@@ -1,2 +1,3 @@
 export * from './error'
 export * from './async'
+export * from './auth-header'
--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from 'express'
 import healthCheckRouter from './health-check'
 import emergencyRouter from './emergency'
 import { asyncHandler } from '../middleware/async'
+import { readHeader } from '../middleware/auth-header'
 
 const router = Router()
 
 router.use('/healthCheck', healthCheckRouter)
 router.use('/emergency', asyncHandler(emergencyRouter))
+router.use('/readHeader', asyncHandler(readHeader))
 
 export default router
