6a64aee82d4eec64952de1a90899767147d9c92b
CalumSmith2401 <60100832+CalumSmith2401@users.noreply.github.com>
2021-04-15 11:26:55 +0800

    minor fix

--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import healthCheckRouter from './health-check'
+import healthcheckRouter from './health-check'
 import emergencyRouter from './emergency'
 import { asyncHandler } from '../middleware/async'
 import { createApiKeyMiddleware } from '../middleware/authMiddleware'
