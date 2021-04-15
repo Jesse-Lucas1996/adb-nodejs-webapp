d437f84254f54a457a2db7ce18874e5140ea5d53
CalumSmith2401 <60100832+CalumSmith2401@users.noreply.github.com>
2021-04-15 11:17:42 +0800

    created apiKeyMiddleware

--- src/api/middleware/authMiddleware.ts
+++ src/api/middleware/authMiddleware.ts
@@ -1,30 +1,12 @@
-import { config } from '../../../config'
-import { Request, Response, NextFunction } from 'express'
+import { Request, Response, NextFunction, RequestHandler } from 'express'
 
-export function authMiddleware(
-  req: Request,
-  res: Response,
-  next: NextFunction
-) {
-  const apiKey = req.header('x-api-key')
+export function createApiKeyMiddleware(apiKey: string): RequestHandler {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const reqApiKey = req.header('x-api-key')
 
-  if (!apiKey || apiKey !== config.apiKey) {
-    return res.status(403).send()
-  }
-  return next()
-}
-
-export function checkAuthMiddleware(apiKey: string) {
-  function checkAuthApi() {
-    return (checkApiKey: string) => checkApiKey === apiKey
+    if (!reqApiKey || reqApiKey !== apiKey) {
+      return res.status(403).send()
+    }
+    return next()
   }
 }
-
-// const checkAuthApiA = checkAuthApi(config.apiKey)
-// const checkAuthApiB = checkAuthApi(config.apiKey2electricboogaloo)
-// const check1 = checkAuthApiA('AEIfieliesnfpiaehfipaeffeoibfibeoifbheoihf')
-// const check2 = checkAuthApiA('poop')
-// const check3 = checkAuthApiB('liehfaefoiewghfewghfoieshfoieshfuoeshf')
-// const check4 = checkAuthApiA('poop')
-
-// console.log(check1, check2, check3, check4)
--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -2,17 +2,15 @@ import { Router } from 'express'
 import healthCheckRouter from './health-check'
 import emergencyRouter from './emergency'
 import { asyncHandler } from '../middleware/async'
-import { authMiddleware } from '../middleware/authMiddleware'
+import { createApiKeyMiddleware } from '../middleware/authMiddleware'
 
 const router = Router()
 
-<<<<<<< HEAD
 router.use('/healthCheck', healthCheckRouter)
-router.use('/emergency', asyncHandler(emergencyRouter))
-router.use('/readHeader', asyncHandler(readHeader))
-=======
-router.use('/healthcheck', healthcheckRouter)
-router.use('/emergency', authMiddleware, asyncHandler(emergencyRouter))
->>>>>>> for you calum
+router.use(
+  '/emergency',
+  createApiKeyMiddleware('API_KEY_HERE'),
+  asyncHandler(emergencyRouter)
+)
 
 export default router
