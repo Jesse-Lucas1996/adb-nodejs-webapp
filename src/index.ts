074cc2d29c7584f35379915955ebfb5ec60aacae
JesseLucasCorpCloud <jlucas@corpcloud.com.au>
2021-04-23 16:12:17 +0800

    * Pug template engine *basic status page

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,28 @@
 import { pool } from './adb'
 import app from './api/create-server'
-
+import path from 'path'
 const port = 3000
+const host = 'localhost'
+
 
 pool.start()
 
+app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, "views"))
+
+app.get('/', (_req,res)=>{
+  // we call our API
+  const mockData = {
+    '1.1.1.1': { state: 'connected' },
+    '1.1.1.2': { state: 'disconnected' },
+    '1.1.1.3': { state: 'connected' },
+    '1.1.1.4': { state: 'disconnected' },
+    '1.1.1.5': { state: 'connected' },
+  }
+
+  res.render('homepage.pug', { mockData: mockData })
+})
+
 app.listen(port, () => {
-  console.log('listening at ', port)
+  console.log('listening at ', port, 'and the host is ', host)
 })
