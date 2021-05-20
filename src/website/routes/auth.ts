cae0ad4975542895cd057fcddab7b0c926a6084b
Jesse Lucas <jlucas@corpcloud.com.au>
2021-05-20 03:03:42 +0000

    Revert "Merge branch 'Database' into 'main'"
    This reverts merge request !11

--- src/website/routes/auth.ts
+++ src/website/routes/auth.ts
@@ -1,6 +1,6 @@
 import express from 'express'
+import { userDb } from '../../database/users'
 import cookieParser from 'cookie-parser'
-import { repo } from '../../database/index'
 const router = express.Router({})
 
 router.use(cookieParser('user'))
@@ -12,15 +12,24 @@ router.get('/', async (_req, res) => {
 router.post('/', function (req, res) {
   const { username, password } = req.body
 
-  if (!repo.validateCredentials(username, password).isValid) {
+  const userExists = Object.keys(userDb).includes(username)
+
+  if (!userExists) {
     return res.status(401).send()
   }
-
-  res.cookie('user', username, {
-    signed: true,
-    maxAge: 864000,
-    httpOnly: true,
-  })
-  return res.status(200).redirect('/homepage')
+  const dbPassword = userDb[username]
+  if (!dbPassword) {
+    return res.status(403).send()
+  }
+  if (password === dbPassword) {
+    res.cookie('user', username, {
+      signed: true,
+      maxAge: 864000,
+      httpOnly: true,
+    })
+    return res.status(200).redirect('/homepage')
+  } else {
+    return res.status(401).send()
+  }
 })
 export default router
